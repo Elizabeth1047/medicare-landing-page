e99d6b3e04b76907872e2955bc98c0cb413caabc
Elizabeth1047 <egharevbalizzy5@gmail.com>
2022-11-08 12:11:55 -0800

    responsiveness solved

---
+++ src/gsap.js
@@ -0,0 +1,293 @@
+ const tl = gsap.timeline({ dafault: {} });
+ 
+tl.from(".imgss", {
+  y: "-50",
+  ease: "elastic(1,0.3)",
+  duration: 1.5,
+  opacity: 0,
+}).from(".gt", {
+  y: "50",
+  ease: "power4.inOut",
+  duration: 1,
+  stagger: 1,
+  opacity: 0,
+}).from(".sesecs", {
+  y: "50",
+  ease: "power4.inOut",
+  duration: 1,
+  stagger: 1,
+  opacity: 0,
+}).from(".s", {
+  x: "-50",
+  ease: "power4.inOut",
+  duration: 1.5,
+  stagger: 1,
+  opacity: 0,
+});
+
+gsap.registerPlugin(ScrollTrigger);
+
+// gsap.from(".dos", {
+//   scrollTrigger: {
+//     trigger: ".imgtexs",
+//     toggleActions: "restart reverse restart reverse",
+//     start: "10% 40%",
+//     end: "40% top",
+//     //  markers: true,
+//   },
+//   x: "-100vw",
+//   ease: "power4.inOut",
+//   opacity: 1,
+//   duration: 2,
+// });
+// gsap.from(".rr", {
+//   scrollTrigger: {
+//     trigger: ".rr",
+//     toggleActions: "restart reverse restart reverse",
+//     start: "10% 40%",
+//     end: "40% top",
+//     // markers: true,
+//   },
+//   y: "-50",
+//   ease: "power4.inOut",
+//   stagger: 1,
+//   opacity:0,
+//   duration: 2,
+// });
+
+gsap.from(".y", {
+  scrollTrigger: {
+    trigger: ".y",
+    toggleActions: "restart reverse restart reverse",
+    start: "10% 40%",
+    end: "250% top",
+    //  markers: true,
+  },
+  x: "-50",
+  ease: "power4.inOut",
+  stagger: 1,
+  opacity: 0,
+  duration: 2,
+});
+gsap.from(".x", {
+  scrollTrigger: {
+    trigger: ".x",
+    toggleActions: "restart reverse restart reverse",
+    start: "10% 40%",
+    end: "250% top",
+    // markers: true,
+  },
+  y: "-50",
+  ease: "power4.inOut",
+  stagger: 1,
+  opacity: 0,
+  duration: 2,
+});
+gsap.from(".z", {
+  scrollTrigger: {
+    trigger: ".z",
+    toggleActions: "restart reverse restart reverse",
+    start: "10% 40%",
+    end: "250% top",
+    // markers: true,
+  },
+  y: "50",
+  ease: "power4.inOut",
+  stagger: 1,
+  opacity: 0,
+  duration: 2,
+});
+
+
+// gsap.from(".yy", {
+//   scrollTrigger: {
+//     trigger: ".y",
+//     toggleActions: "restart reverse restart reverse",
+//     start: "550% center",
+//     end: "600% top",
+//     // markers: true,
+//   },
+//   x: "-50",
+//   ease: "power4.inOut",
+//   stagger: 1,
+//   opacity: 0,
+//   duration: 2,
+// });
+// gsap.from(".xx", {
+//   scrollTrigger: {
+//     trigger: ".y",
+//     toggleActions: "restart reverse restart reverse",
+//     start: "550% center",
+//     end: "600% top",
+//     // markers: true,
+//   },
+//   y: "50",
+//   ease: "power4.inOut",
+//   stagger: 1,
+//   opacity: 0,
+//   duration: 2,
+// });
+// gsap.from(".zz", {
+//   scrollTrigger: {
+//     trigger: ".y",
+//     toggleActions: "restart reverse restart reverse",
+//     start: "550% center",
+//     end: "600% top",
+//     // markers: true,
+//   },
+//   y: "-50",
+//   ease: "power4.inOut",
+//   stagger: 1,
+//   opacity: 0,
+//   duration: 2,
+// });
+gsap.from(".dd", {
+  scrollTrigger: {
+    trigger: ".y",
+    toggleActions: "restart reverse restart reverse",
+    start: "750% center",
+    end: "800% top",
+    // markers: true,
+  },
+  x: "-100",
+  ease: "power4.inOut",
+
+  opacity: 0,
+  duration: 3,
+});
+
+// gsap.from(".omm", {
+//   scrollTrigger: {
+//     trigger: ".y",
+//     toggleActions: "restart reverse restart reverse",
+//     start: "1000% center",
+//     end: "1050% top",
+//     //  markers: true,
+//   },
+//   y: "50",
+//   ease: "power4.inOut",
+//   stagger: 1,
+//   opacity: 0,
+//   duration: 2,
+// });
+// gsap.from(".myca", {
+//   scrollTrigger: {
+//     trigger: ".y",
+//     toggleActions: "restart reverse restart reverse",
+//     start: "1350% center",
+//     end: "1400% top",
+//     // markers: true,
+//   },
+//   x: "-100",
+//   ease: "power4.inOut",
+//   stagger: 1,
+
+//   opacity: 0,
+//   duration: 1,
+// });
+gsap.from(".aa", {
+  scrollTrigger: {
+    trigger: ".aa",
+    toggleActions: "restart reverse restart reverse",
+    start: "-20% center",
+    end: "10% top",
+    // markers: true,
+  },
+  x: "-100",
+  ease: "power4.inOut",
+  stagger: 1,
+
+  opacity: 0,
+  duration: 1,
+});
+gsap.from(".bb", {
+  scrollTrigger: {
+    trigger: ".bb",
+    toggleActions: "restart reverse restart reverse",
+    start: "-20% center",
+    end: "10% top",
+    // markers: true,
+  },
+  y: "50",
+  ease: "power4.inOut",
+  stagger: 1,
+
+  opacity: 0,
+  duration: 1,
+});
+gsap.from(".cc", {
+  scrollTrigger: {
+    trigger: ".cc",
+    toggleActions: "restart reverse restart reverse",
+    start: "-20% center",
+    end: "10% top",
+    // markers: true,
+  },
+  y: "-50",
+  ease: "power4.inOut",
+  stagger: 1,
+  opacity: 0,
+  duration: 1,
+});
+
+// gsap.from(".dd", {
+//   scrollTrigger: {
+//     trigger: ".dd",
+//     toggleActions: "restart reverse restart reverse",
+//     start: "550% center",
+//     end: "650% top",
+//     markers: true,
+//   },
+//   y: "50",
+//   ease: "power4.inOut",
+//   stagger: 1,
+//   opacity: 0,
+//   duration: 1,
+// });
+
+// gsap.from(".ee", {
+//   scrollTrigger: {
+//     trigger: ".ee",
+//     toggleActions: "restart reverse restart reverse",
+//     start: "550% center",
+//     end: "650% top",
+//     // markers: true,
+//   },
+//   y: "-50",
+//   ease: "power4.inOut",
+//   stagger: 1,
+
+//   opacity: 0,
+//   duration: 1,
+// });
+// gsap.from(".ff", {
+//   scrollTrigger: {
+//     trigger: ".ff",
+//     toggleActions: "restart reverse restart reverse",
+//     start: "550% center",
+//     end: "650% top",
+//     // markers: true,
+//   },
+//   y: "50",
+//   ease: "power4.inOut",
+//   stagger: 1,
+
+//   opacity: 0,
+//   duration: 1,
+// });
+// gsap.from(".gg", {
+//   scrollTrigger: {
+//     trigger: ".cc",
+//     toggleActions: "restart reverse restart reverse",
+//     start: "550% center",
+//     end: "650% top",
+//     // markers: true,
+//   },
+//   x: "-50",
+//   ease: "power4.inOut",
+//   stagger: 1,
+
+//   opacity: 0,
+//   duration: 1,
+// });
+
--- src/script.js
+++ src/script.js
@@ -1,72 +1,128 @@
-//  let arr = Array.from(document.querySelectorAll(".hove"));
+let pointer = document.querySelector(".upst");
+let secsss = document.querySelector(".secs");
+console.log(secsss);
+
+pointer.addEventListener("click", () => {
+  secsss.classList.toggle("shown");
+  }
+);
 
 let secss = document.querySelector(".secs");
 let arr = Array.from(secss.children);
 let forward = document.getElementById("forward");
+let forward2 = document.getElementById("for");
+console.log(forward2);
 let backward = document.getElementById("backward");
+let backward2 = document.getElementById("back");
+console.log(backward2);
+let gen2 = document.querySelector(".myc");
+console.log(gen2);
 let gen = document.querySelector(".omn");
 let image = Array.from(gen.children);
 let firstdot = document.getElementsByClassName("b");
-let dots = Array.from(firstdot);
+let seconddot = document.getElementsByClassName("l");
+ console.log(seconddot);
+let spots = 0;
+let spotss = 0;
+let respot = 6;
+let respotss=3;
+
+
+function enterspot() {
+  let dots = Array.from(firstdot);
+  dots[spots].classList.add("blues");
+  spots += 1;
+
+}
+function enterspot1() {
+  let dotes = Array.from(seconddot);
+  dotes[spotss].classList.add("blues");
+  spotss += 1;
+}
+
+function removespot() {
+  let dots = Array.from(firstdot);
+  respot -= 1;
+  console.log(respot);
+  dots[respot].classList.remove("blues");
+}
+function removespot2() {
+  let dotes = Array.from(seconddot);
+  respotss -= 1;
+  console.log(respot);
+  dotes[respotss].classList.remove("blues");
+}
+
+arr.map((ar) => {
+  ar.addEventListener("mouseover", () => {
+    ar.classList.add("line");
+  });
+});
+
+arr.map((ar) => {
+  ar.addEventListener("mouseout", () => {
+    ar.classList.remove("line");
+  });
+});
+let count = 25;
+let count2 = 32;
+let dr = 4;
+let imagex=image.length
+console.log(imagex)
+let drr = 3;
 
 
 
-arr.map((ar)=>{
-        ar.addEventListener("mouseover", () => {
-          ar.classList.add("line");
+ function forwar() {
+   gen.style.left = -count + "vw";
+   count += 22;
+  enterspot();
+   
+ }
 
-        });
- })
+ function forwar2() {
+   gen2.style.left = -count2 + "vw";
+   count2 += 30;
+   enterspot1();
+ }
 
- arr.map((ar) => {
-   ar.addEventListener("mouseout", () => {
-     ar.classList.remove("line");
-   });
- });
-
- 
-  
-    let x = image.length;
-    console.log(x);
-    let gr = 25;
-    let tr = 5;
-    let dr = 4;
-    
-      forward.addEventListener("click", () => {
-        if (dr >= 4 && dr <= 8) {
-          dr++;
-          dots.forEach((dot) => {
-
-            dot.nextElementSibling.style.background
-          });
-
-          b.nextElementSibling
-           console.log("strge");
-          gen.style.left = -gr + "vw";
-          let boss = gr + 22;
-          gr=boss
-
-        } else {
-          console.log("strange");
-        }
-
-        
-      })
-//       backward.addEventListener("click", () => {
-//         if (dr >= 4 && dr <= 9) {
-//           dr--;
-//           let bosss = -tr - 25;
-// console.log("strge");
-//           gen.style.left = tr + "vw";
-          
-//           tr = bosss;
-//         } else {
-//           console.log("strange");
-//         }
-//       });
-    
+function backwar() {
+  console.log(count);
+  count += -22;
+  gen.style.left = -count + "vw";
+  removespot();
+} 
 
 
-  
- 
-  
+function backwar2() {
+  console.log(count2);
+  count2 += -30;
+  gen2.style.left = -count2 + "vw";
+  removespot2();
+}
+
+forward.addEventListener("click", () => {
+  if ( dr <= 8) {
+    dr++;
+    forwar(gen,count,22);
+  }
+});
+forward2.addEventListener("click", () => {
+  if ( drr <= 4) {
+    drr++;
+    forwar2(gen2, count2, 30);
+  }
+});
+
+backward.addEventListener("click", () => {
+  if (dr >= 4 ) {
+    dr--;
+    backwar();
+  }
+});
+backward2.addEventListener("click", () => {
+  if (drr >= 3 ) {
+    drr--;
+    backwar2();
+  }
+});
